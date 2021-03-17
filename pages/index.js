bfb341a0a989cac5e235cd1cde76bb6abcf97dcf
Tyler Nix <tylernix@pm.me>
2021-03-17 17:09:20 -0500

    init mars-app

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,60 @@
 import Head from 'next/head'
+import { useUser } from '@auth0/nextjs-auth0';
+import LinkCard from '../components/link-card';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  let logInCard;
+  if (user) {
+    logInCard = <LinkCard url="/api/auth/logout" title="Log Out" description={'Thanks ' + user.email + ' for visiting! Hope to see you again.'} />
+  } else {
+    logInCard = <LinkCard url="/api/auth/login" title="Log In" description="Get the most out of our planet by logging in!" />
+  }
+  
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Welcome to Mars</title>
+        <link rel="icon" href="/mars-favicon.ico" />
       </Head>
+      <Header />
+      <main className="flex flex-col w-full items-center justify-center flex-1 px-20 text-center bg-mars-landscape bg-cover">
+        <div className="bg-gray-100 bg-opacity-70 p-6 px-24 rounded-xl flex flex-col items-center">
+          {/* <img src="/img/mars-logo.png" className="w-32 h-32"></img> */}
+          <h1 className="text-6xl font-bold">
+            Welcome to{' '}
+            <span className="text-blue-600">
+              Mars!
+            </span>
+          </h1>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+          <p className="mt-3 text-2xl">
+            We are about{' '}
+            <code className="p-2 font-mono text-lg border-2 border-blue-600 rounded-md">
+              140 million miles
+            </code>
+            {' '}from earth.
+          </p>
+          <p className="mt-3 text-2xl">
+          Come visit us soon.
+          </p>
+        </div>
+        
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="flex flex-wrap items-center justify-around max-w-3xl mt-6 sm:w-full">
+          <LinkCard url="/about" title="About Us" description="Learn about all the amazing things our planet has to offer." />
+          { logInCard }
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer />
     </div>
   )
 }
